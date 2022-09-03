7ff0dd9372e4d49661c3dacc989eb7946f3418f6
frenchkebab <tubowangin@gmail.com>
2022-09-03 15:54:38 +0900

    adds DAI_WHALE address

--- test/unlock-account.js
+++ test/unlock-account.js
@@ -2,6 +2,7 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+const DAI_WHALE = '0x2FAF487A4414Fe77e2327F0bf4AE2a264a776AD2';
 
 describe('Test unlock account', () => {
   let accounts;
